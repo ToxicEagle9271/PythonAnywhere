4721f34529a98ba384a539ce80855cb3fddbc703
ToxicEagle9271 <jcoronamedina@unomaha.edu>
2024-02-17 13:08:01 -0600

    files

--- locallibary/settings.py
+++ locallibary/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p07jg!ket797p0rs&u^f(r2si^p^76sg_e4-zj(oea3yxak=qv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
